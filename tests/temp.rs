68b041c157eae0d8c2f88a42a8058066a35552ea
N-Adda <nbourass@insa-rennes.fr>
2025-11-18 14:28:08 +0100

    Test integration version 1 not working totally

--- tests/temp.rs
+++ tests/temp.rs
@@ -1,24 +1,51 @@
 #[cfg(test)]
 mod tests {
+    use image::RgbImage;
+    use image::ImageReader;
+    use moseiik::main::compute_mosaic;
+
     #[test]
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     fn test_x86() {
         // Fonction compute mosaic prend en arg un type Options
 
         let args = moseiik::main::Options {
-            image: "assets/kit.jpeg",
-            output: "tests/x86img.png",
-            tiles: "assets",
+            image: "assets/kit.jpeg".to_string(),
+            output: "tests/x86img.png".to_string(),
+            tiles: "tests/test_images".to_string(),
             scaling: 1,
             tile_size: 25,
             remove_used: false,
             verbose: true,
             simd: true,
             num_thread: 1,
-        }
+        };
+
+        compute_mosaic(args);
+
+        // image générée
+        let img1: RgbImage = match ImageReader::open("tests/x86img.png") {
+            Ok(i) => match i.decode() { // Etape decodage
+                Ok(img) => img.into_rgb8(), // Etape conversion en RgbImage comme la fonction prepare_target
+                Err(_) => panic!("Erreur lors du décodage de l'image"),
+            },
+            Err(_) => panic!("Erreur lors de la conversion en RgbImage"),
+        };
+        // image base
+        let img2: RgbImage = match ImageReader::open("assets/ground-truth-kit.png") {
+            Ok(i) => match i.decode() { // Etape decodage
+                Ok(img) => img.into_rgb8(), // Etape conversion en RgbImage comme la fonction prepare_target
+                Err(_) => panic!("Erreur lors du décodage de l'image"),
+            },
+            Err(_) => panic!("Erreur lors de la conversion en RgbImage"),
+        };
+
+        let (w_ref,h_ref) = img2.dimensions();
+        let (w_test,h_test) = img1.dimensions();
+        assert_eq!(w_ref, w_test, "Largeur des images différente");
+        assert_eq!(h_ref, h_test, "Hauteur des images différente");
 
         // test avx2 or sse2 if available
-        assert!(false);
     }
 
     #[test]
